f6b463d41e865c50ff2b8c0b444bbd89d01b30da
dso <adam.pridgen@thecoverofnight.com>
2017-09-16 23:49:54 -0500

    elk starts and runs correctly

--- elk-secx/run-file.sh
+++ elk-secx/run-file.sh
@@ -1,4 +1,4 @@
-DOCKER_TAG=elk_docker:latest
+DOCKER_TAG=elkdocker_elk:latest
 DOCKER_NAME=elk-secx
 
 # setup dirs
@@ -10,8 +10,8 @@ DOCKER_DATA=$DOCKER_NB/data
 EXPOSED_JSON_PORT='5002'
 JSON_PORT='5002/udp'
 DOCKER_JSON_PORT=$EXPOSED_JSON_PORT:$JSON_PORT
-DOCKER_PORTS='-p $DOCKER_JSON_PORT -p 5601:5601 -p 9200:9200 -p 5044:5044 '
-DOCKER_ENV='-e ES_HEAP_SIZE="4g" -e LS_HEAP_SIZE="4g"'
+DOCKER_PORTS="-p $DOCKER_JSON_PORT -p 5601:5601 -p 9200:9200 -p 5044:5044 "
+DOCKER_ENV="-e ES_HEAP_SIZE\=\"4g\" -e LS_HEAP_SIZE\=\"4g\""
 DOCKER_VOL=''
 
 sudo mkdir -p $DOCKER_DATA
@@ -19,5 +19,7 @@ sudo mkdir -p $DOCKER_LOGS
 sudo chmod -R a+rw $DOCKER_NB
 
 # run command not 
+echo "docker run $DOCKER_PORTS $DOCKER_VOL -it  $DOCKER_ENV \
+           --name $DOCKER_NAME $DOCKER_TAG"
 docker run $DOCKER_PORTS $DOCKER_VOL -it  $DOCKER_ENV \
            --name $DOCKER_NAME $DOCKER_TAG
