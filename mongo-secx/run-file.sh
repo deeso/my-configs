8fb06bb4586d9be83bc44690d24fb2c8699a608a
dso <adam.pridgen@thecoverofnight.com>
2017-09-17 00:14:24 -0500

    adding mongo docker used to store syslog data

---
+++ mongo-secx/run-file.sh
@@ -0,0 +1,22 @@
+DOCKER_TAG=mongo:latest
+DOCKER_NAME=mongo-secx
+
+# setup dirs
+DOCKER_BASE=/data
+DOCKER_NB=$DOCKER_BASE/$DOCKER_NAME
+DOCKER_LOGS=$DOCKER_NB/logs
+DOCKER_DATA=$DOCKER_NB/data
+
+DOCKER_PORTS="-p 27017:27017 "
+DOCKER_ENV=""
+DOCKER_VOL="-v /opt/mongodb/data/db:/data/db -v /opt/mongodb/data/configdb:/data/configdb "
+
+sudo mkdir -p $DOCKER_DATA
+sudo mkdir -p $DOCKER_LOGS
+sudo chmod -R a+rw $DOCKER_NB
+
+docker build -t $DOCKER_TAG .
+# run command not 
+docker run $DOCKER_PORTS $DOCKER_VOL -it  $DOCKER_ENV \
+           --name $DOCKER_NAME $DOCKER_TAG
+
